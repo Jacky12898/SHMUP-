0897979dbc30cc857740b908a7c3d90cfa61e3d9
Jacky12898 <32112544+Jacky12898@users.noreply.github.com>
2019-09-30 12:09:06 -0600

    Shmup update

--- Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
+++ Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour
 {
     /// <summary>
-    /// Keeps a GameObject on screen.
+    /// Checks whether a GameObject is on screen and can force it to stay on screen.
     /// Note that this only works for an orthographic Main Camera at [0,0,0].
     /// </summary>
 
     [Header("Set in Inspector")]
     public float radius = 4f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     void Awake()
     {
@@ -25,25 +30,44 @@ public class BoundsCheck : MonoBehaviour
     void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
-        if(pos.x > camWidth - radius)
+        if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
 
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
 
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
 
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
+        }
+
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
         }
 
         transform.position = pos;
